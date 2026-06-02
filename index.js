b0c4e1d12a69f43443c02d12c762182b136e5db7
RakshithaRMogaveera <rakshitharmogaveera18@gmail.com>
2026-06-02 18:56:16 +0530

    Render deployment fix

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
-const express = require('express');
-const dbconnection = require('./db');
-const cors = require('cors');
-const path = require('path');
+
+const express = require("express");
+const dbconnection = require("./db");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-const PORTNUMBER = 7000;
+const PORT = process.env.PORT || 7000;
 
 // DATABASE CONNECTION
 dbconnection();
@@ -19,17 +20,17 @@ app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "Uploads")));
 
 // ROUTES
-app.use('/user', require('./Routes/UserRoutes'));
-app.use('/product', require('./Routes/ProductRoutes'));
-app.use('/category', require('./Routes/category_routes'));
-app.use('/booking', require('./Routes/booking_route'));
+app.use("/user", require("./Routes/UserRoutes"));
+app.use("/product", require("./Routes/ProductRoutes"));
+app.use("/category", require("./Routes/category_routes"));
+app.use("/booking", require("./Routes/booking_route"));
 
 // TEST API
-app.get('/apitest', (req, res) => {
-    res.send("Hello from express");
+app.get("/apitest", (req, res) => {
+  res.send("Hello from express");
 });
 
 // SERVER
-app.listen(PORTNUMBER, () => {
-    console.log(`server is running on portnumber:${PORTNUMBER}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
